bcb368ae3d2f34f51214c5f55734487921b78b8b
wilhelmpaulm <wilhelmpaulm@gmail.com>
2013-05-25 12:16:16 +0800

    eddited a few stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,37 @@
 class PostsController < ApplicationController
 	def new 
+		@post = Post.new
 	end
+
+	def index
+		@posts = Post.all
+	end	
 	
+	def create
+		@post = Post.new(params[:post].permit(:title, :text))
+ 
+		if @post.save
+		redirect_to @post
+		else 
+		render "new"
+		end 
+	end
+
+	def show
+  		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+	  @post = Post.find(params[:id])
+	 
+	  if @post.update(params[:post].permit(:title, :text))
+	    redirect_to @post
+	  else
+	    render 'edit'
+	  end
+	end
 end
